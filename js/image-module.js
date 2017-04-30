a420a83ffe0ec554071028b9c6e667d7284511fc
Cameron <mooreheadc@hotmail.com>
2017-04-30 15:03:35 -0700

    Finished image slider

--- js/image-module.js
+++ js/image-module.js
@@ -22,15 +22,63 @@ $(document).ready(() => {
 function imageModal(title, des, path, key) {
   let modal =
     `<div class="modal">
-      <span class="prev">&larr;</span>
+      <span class="prev" onclick="prevImage('${key}')">&larr;</span>
       <h3 class="modal-title">${title}</h3>
       <img src=${path} class="modal-image">
       <p>${des}</p>
-      <span class="next">&rarr;</span>
+      <span class="next" onclick="nextImage('${key}')">&rarr;</span>
       <span class="exit" onclick="exitModal()">&times;</span>
     </div>`
   $("body").append("<div class='blackout'></div>")
   $("body").append(modal)
+}
+
+function prevImage(key) {
+  const jsonURL = "data/image-module-data.json"
+  $.getJSON(jsonURL, function(json) {
+    let imageArray = json.image_module
+    let prevKey = parseInt(key) - 1
+    let imageDataObject = {}
+    // provide image slider looping
+    if (parseInt(key) === 0) {
+      prevKey = imageArray.length -1
+      imageDataObject = imageArray[prevKey]
+    } else {
+      imageDataObject = imageArray[prevKey]
+    }
+    // clear from DOM for memory
+    exitModal()
+    return imageModal(
+      imageDataObject.title,
+      imageDataObject.description,
+      imageDataObject.path_name,
+      imageDataObject.key
+    )
+  })
+}
+
+function nextImage(key) {
+  const jsonURL = "data/image-module-data.json"
+  $.getJSON(jsonURL, function(json) {
+    let imageArray = json.image_module
+    let nextKey = parseInt(key) + 1
+    let imageDataObject = {}
+    // provide image slider looping
+    if (parseInt(key) === imageArray.length -1) {
+      nextKey = 0
+      imageDataObject = imageArray[nextKey]
+    } else {
+      imageDataObject = imageArray[nextKey]
+    }
+    // clear from DOM for memory
+    exitModal()
+    return imageModal(
+      imageDataObject.title,
+      imageDataObject.description,
+      imageDataObject.path_name,
+      imageDataObject.key
+    )
+  })
 }
 
 function exitModal() {
